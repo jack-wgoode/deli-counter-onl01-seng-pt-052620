f5e5a74dd22160a7042189eb228c309a1be90128
Jack Goode <jack.w.goode@icloud.com>
2020-06-06 22:55:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
        puts "The line is currently empty."
   else  
     katz_deli.each do |person| 
-      array << " #{i+1}. #{person}" 
+      array << "#{i+1}. #{person}" 
       i +=1
       end
       puts "#{sentence} #{array.join}"
