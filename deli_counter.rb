2716f4cbd8ecde3f7900330fb5577d3fee4ee0dc
Jack Goode <jack.w.goode@icloud.com>
2020-06-06 22:25:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(katz_deli)
   if katz_deli.length == 0
-     puts "The line is currently empty."
-       else puts "The line is currently:"
+       puts "The line is currently empty."
+  else puts "The line is currently:" 
     katz_deli.each.with_index(1) {|person, index| puts " #{index}. #{person} "}
   end
 end
@@ -13,5 +13,5 @@ def take_a_number(katz_deli, name)
 end
  
  def now_serving(katz_deli)
-     katz_deli.length == 0 ? puts "There is nobody waiting to be served!" : puts "Currently serving #{katz_deli.shift}"
+     katz_deli.length == 0 ?  (puts "There is nobody waiting to be served!") : (puts "Currently serving #{katz_deli.shift}.")
  end
